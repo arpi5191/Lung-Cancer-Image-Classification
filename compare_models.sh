f61fceb2b6998951b648b5206e274c533c3a9277
Arpitha Rao <69826982+arpi5191@users.noreply.github.com>
2025-09-04 01:40:12 -0700

    Update compare_models.sh
    Adding compare_models.sh version with times

--- compare_models.sh
+++ compare_models.sh
@@ -29,7 +29,7 @@ run_patch_tests() {
 }
 
 # Set the number of runs per patch type
-num_simulations=20
+num_simulations=1
 
 # Run tests for both tumor and voronoi patches, storing results in respective arrays
 run_patch_tests "tumor" original_results $num_simulations
@@ -54,3 +54,9 @@ echo "Running Wilcoxon test..."
 python wilcoxon_test.py --original "$original_json" --voronoi "$voronoi_json"
 echo "Finished Wilcoxon test."
 echo " "
+
+# Time for 1 simulation on Bash script w/2 epochs per input type: 16 mins 25 secs
+
+# Time for 1 simulation on Bash script w/1 epoch per input type: 8 mins
+# Time for 1 simulation on Bash script w/30 epochs per input type: 4 hrs
+# Time for 20 simulations on Bash script w/30 epochs per input type: 80 hrs
